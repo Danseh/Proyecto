ff9bc9735dde5e9c0203e3372f2e29d27f731f17
Danseh <xxfirox123@gmail.com>
2022-05-16 22:28:13 +0200

    arreglo login

--- assets/components/Aside.jsx
+++ assets/components/Aside.jsx
@@ -4,13 +4,12 @@ import Login from './Login';
 import Panel from './Panel';
 
 const myStorage2 = window.localStorage;
-const logoutUrl =  `/logout`;
 
 const Aside = ({userGlobal,setUserGlobal}) => {
 
   return (
     <aside>
-      {userGlobal ? <Panel userGlobal={userGlobal}/> : <Login setUserGlobal={setUserGlobal}/>}
+      {userGlobal ? <Panel userGlobal={userGlobal} setUserGlobal={setUserGlobal}/> : <Login setUserGlobal={setUserGlobal}/>}
     </aside>
   )
 }
